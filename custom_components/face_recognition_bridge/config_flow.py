c5146ecb3f0a644b7c67aec9987eaf4f7bba6f43
WWWorm92 <yadanbryvko@gmail.com>
2026-07-15 11:57:58 +0300

    12

--- custom_components/face_recognition_bridge/config_flow.py
+++ custom_components/face_recognition_bridge/config_flow.py
@@ -4,6 +4,7 @@ import voluptuous as vol
 
 from homeassistant import config_entries
 
+from .api import FaceRecognitionBridgeApi
 from .const import CONF_API_TOKEN, CONF_BASE_URL, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL, DOMAIN
 
 
@@ -11,8 +12,21 @@ class FaceRecognitionBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
-            return self.async_create_entry(title="Face Recognition Bridge", data=user_input)
+            normalized_url = user_input[CONF_BASE_URL].rstrip("/")
+            await self.async_set_unique_id(normalized_url)
+            self._abort_if_unique_id_configured()
+
+            api = FaceRecognitionBridgeApi(normalized_url, user_input.get(CONF_API_TOKEN, ""))
+            try:
+                await self.hass.async_add_executor_job(api.get_health)
+            except Exception:
+                errors["base"] = "cannot_connect"
+            else:
+                user_input[CONF_BASE_URL] = normalized_url
+                return self.async_create_entry(title="Face Recognition Bridge", data=user_input)
 
         schema = vol.Schema(
             {
@@ -21,4 +35,4 @@ class FaceRecognitionBridgeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): int,
             }
         )
-        return self.async_show_form(step_id="user", data_schema=schema)
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
